c77d38c48bee67ece372cc593669145954656139
Zach Posten <zposten@gmail.com>
2015-12-17 20:53:42 -0600

    Parse url to get itrs param

--- main_node.js
+++ main_node.js
@@ -1,20 +1,25 @@
 var http = require('http');
 var pi = require('./piMaker.js');
+var url = require('url');
+var script = require('./script.js');
+
+var port = 8080;
 
 
 http.createServer(function (req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    var params = script.parseUrl(req.url);
 
     var itrs = 100;
-    if(req.hasOwnProperty('itrs')) {
-        itrs = Number(req.query.itrs);
+    if(params && params.itrs) {
+        itrs = Number(params.itrs);
     }
 
     var strResponse = pi.nilakPi(itrs).toString() + ", " + pi.fftPi(itrs).toString();
     res.write(strResponse);
     res.end();
 
-}).listen(8080);
+}).listen(port);
 
-console.log('Server running at http://127.0.0.1:8080');
+console.log('Server running at http://127.0.0.1:' + port);
--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-window.onload = function() {
-	
-};
 
 
-function displayPi() {
-	var itrs = document.getElementById("iterations").value;
-    itrs = parseInt(itrs);
 
-    document.getElementById("display_nilak").innerText = nilakPi(itrs).toString();
-    document.getElementById("display_fft").innerText = fftPi(itrs).toString();
+/**
+ * This function is meant to handle a url as returned by node's http.ClientRequest
+ * "/?itrs=3&zach=awesome"
+ * @param url
+ */
+module.exports.parseUrl = function(url) {
+    var query = url.substring(url.indexOf("?") + 1);
+    var params = query.split("&");
+
+    var objParams = {};
+    for(var i=0; i<params.length; ++i) {
+        var param = params[i];
+        var eql = param.indexOf("=");
+        var key = param.substring(0, eql);
+        objParams[key] = param.substring(eql + 1);
+    }
+    return objParams;
 }
